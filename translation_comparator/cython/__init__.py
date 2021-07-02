5fa597f6baf37996628401f8826c890c0a82f863
0dminnimda <0dminnimda@gmail.com>
2021-07-02 09:58:22 +0300

    Update cython\__init__.py

--- translation_comparator/cython/__init__.py
+++ translation_comparator/cython/__init__.py
@@ -6,3 +6,12 @@ from .annotated_html_parser import (get_code_from_two_files,
                                     get_code_from_two_files_by_path)
 from .code_comparator import (compare_and_save_two_files,
                               compare_and_save_two_files_by_path)
+from .path_builders import build_via_suffix_change
+
+__all__ = (
+    "settings",
+    "get_code_from_two_files",
+    "get_code_from_two_files_by_path",
+    "compare_and_save_two_files",
+    "compare_and_save_two_files_by_path",
+    "build_via_suffix_change")
