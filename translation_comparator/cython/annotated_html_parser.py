9be1e737880569aa51c7b122d6883e6d44a50dc3
0dminnimda <0dminnimda@gmail.com>
2021-07-02 09:07:56 +0300

    annotated_html_parser.py: add get_code_from_two_files_by_path

--- translation_comparator/cython/annotated_html_parser.py
+++ translation_comparator/cython/annotated_html_parser.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from pathlib import Path
+from typing import List, Tuple
+
 from bs4 import BeautifulSoup
 from bs4.element import Tag
 
@@ -15,3 +17,13 @@ def get_soup_from_html(path: Path) -> BeautifulSoup:
     html = path.with_suffix(".html").read_text()
 
     return BeautifulSoup(html, "lxml")
+
+
+def get_code_from_two_files_by_path(
+    path1: Path, path2: Path
+) -> Tuple[List[str], List[str]]:
+
+    return (
+        get_code_from_soup(get_soup_from_html(path1)),
+        get_code_from_soup(get_soup_from_html(path2)))
+
