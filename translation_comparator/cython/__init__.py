db41a7d4977623abfb16d2e9c49cb83cb8751348
0dminnimda <0dminnimda@gmail.com>
2021-07-02 09:20:41 +0300

    cython\__init__: import settings, get_code_from_two_files, get_code_from_two_files_by_path

--- translation_comparator/cython/__init__.py
+++ translation_comparator/cython/__init__.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from . import settings
+from .annotated_html_parser import (get_code_from_two_files,
+                                    get_code_from_two_files_by_path)
