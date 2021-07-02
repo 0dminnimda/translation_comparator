7d2128ca9b03d3a9975ca90391d73bf68813209f
0dminnimda <0dminnimda@gmail.com>
2021-07-02 09:34:31 +0300

    Update __init__.py

--- translation_comparator/cython/__init__.py
+++ translation_comparator/cython/__init__.py
@@ -4,3 +4,5 @@
 from . import settings
 from .annotated_html_parser import (get_code_from_two_files,
                                     get_code_from_two_files_by_path)
+from .code_comparator import (compare_and_save_two_files,
+                              compare_and_save_two_files_by_path)
