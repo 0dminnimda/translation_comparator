3c4fb430fe1a1d2196c443650b4e3f4058feb027
0dminnimda <0dminnimda@gmail.com>
2021-07-02 09:29:14 +0300

    code_comparator: add equate_similar_lines, repl, diff_to_str, settings.replace_threshold

--- translation_comparator/cython/code_comparator.py
+++ translation_comparator/cython/code_comparator.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import re
 from pathlib import Path
+from re import Match
 
 from ..typedefs import DIFF, DIFF_FUNC, DIFF_ITER
 from . import settings
 from .annotated_html_parser import get_code_from_two_files_by_path
+
+
+def diff_to_str(diff: DIFF) -> str:
+    return settings.str_between_lines.join(["\n".join(i) for i in diff])
 
 
 def make_diff(func: DIFF_FUNC, iterable: DIFF_ITER) -> DIFF:
@@ -14,8 +20,26 @@ def make_diff(func: DIFF_FUNC, iterable: DIFF_ITER) -> DIFF:
         for a, b in iterable]
 
 
+def repl(match: Match) -> str:
+    if (
+        0 < match.group(2).count("^") < settings.replace_threshold and
+        0 < match.group(4).count("^") < settings.replace_threshold
+    ):
+        return f"  {match.group(1)}"
+
+    return match.group(0)
+
+
+def equate_similar_lines(string: str) -> str:
+    pattern = re.compile(r"- (.+)\n\? (.+)\n\n\+ (.+)\n\? (.+)")
+
+    return pattern.sub(repl, string)
+
+
 def compare_and_save_two_files_by_path(path1: Path, path2: Path) -> None:
     code1, code2 = get_code_from_two_files_by_path(path1, path2)
 
     compare = make_diff(settings.differ.compare, zip(code1, code2))
+
+    compare_str = equate_similar_lines(diff_to_str(compare))
 
--- translation_comparator/cython/settings.py
+++ translation_comparator/cython/settings.py
@@ -2,5 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from difflib import Differ
+replace_threshold: int = 3
+str_between_lines: str = "\n  " + "#"*100 + "\n  "*2
+
 
 differ: Differ = Differ()
