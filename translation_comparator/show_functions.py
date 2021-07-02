3ce266c8dc1b95d49717d228d68aa7dd45ff63c5
0dminnimda <0dminnimda@gmail.com>
2021-07-02 09:57:28 +0300

    show_functions: add show_via_vscode

--- translation_comparator/show_functions.py
+++ translation_comparator/show_functions.py
@@ -8,6 +8,13 @@ import warnings
 from .typedefs import SHOW_FUNC_DATA
 
 
+def show_via_vscode(save_as_diff: bool, data: SHOW_FUNC_DATA) -> None:
+    if save_as_diff:
+        os.system(f"code {data[0]}")  # open file in vscode
+    else:
+        os.system(f"code --diff {data[0]} {data[1]}")  # open file diff
+
+
 def show_via_cmd(save_as_diff: bool, data: SHOW_FUNC_DATA) -> None:
     if sys.platform == "win32":
         if save_as_diff:
