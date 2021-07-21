5bfd8bd93b55dc0dd27cc940348d4d09cdae8c02
0dminnimda <0dminnimda@gmail.com>
2021-07-21 06:30:57 +0300

    path_helpers: add change_same_paths_if_needed

--- translation_comparator/path_helpers.py
+++ translation_comparator/path_helpers.py
@@ -3,7 +3,9 @@
 
 from glob import iglob
 from pathlib import Path
-from typing import Iterator
+from typing import Iterator, Tuple
+
+from .cython.path_builders import unique_stem_via_suffix
 
 
 def self_glob(path: Path) -> Iterator[Path]:
@@ -21,4 +23,16 @@ def relative_to_cwd(path: Path) -> Path:
 
 def with_parent(path: Path, directory: Path) -> Path:
     return directory.joinpath(path.name)
+
+
+def change_same_paths_if_needed(path1: Path, path2: Path) -> Tuple[Path, Path]:
+    if path1.stem == path2.stem:
+        return (unique_stem_via_suffix(path1),
+                unique_stem_via_suffix(path2))
+
+    if path1.name == path2.name:
+        return (path1.with_name(path1.name + " (0)"),
+                path2.with_name(path2.name + " (1)"))
+
+    return (path1, path2)
 
